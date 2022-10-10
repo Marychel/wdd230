cfdb88c5351ef06ae443d34e6528620b1553e0ed
Marychel <cal20024@byui.edu>
2022-10-11 00:08:15 +0900

    update

---
+++ lesson4/scripts/date.js
@@ -0,0 +1,43 @@
+document.querySelector("#year").textContent = new Date().getFullYear();
+
+document.getElementById("lastModif").textContent = `Last Modification:  ${document.lastModified}`;
+
+
+//current date
+let day = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday'
+];
+let month = [
+  'January',
+  'Febuary',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
+let d = new Date();
+let dayName = day[d.getDay()];
+let monthName = month[d.getMonth()];
+let year = d.getFullYear();
+let currentDate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
+document.getElementById('currentDate').textContent = currentDate;
+//nav
+const hambutton = document.querySelector('.hamburgerBtn');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
