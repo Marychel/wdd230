91348d3205762111af4730300ed14a5f18ecbf22
Marychel <cal20024@byui.edu>
2022-10-17 11:26:20 +0900

    update

--- chamber/scripts/date.js
+++ chamber/scripts/date.js
@@ -11,6 +11,16 @@ menuBtn.addEventListener('click', () =>
 
 window.onresize = () => {if (window.innerWidth > 760) mnav.classList.remove('open')};
 
+
+// set banner for monday/tuesday
+let dow = date.getDay();
+if (dow == 1 | dow ==2) {
+    let banner = document.getElementById("banner");
+    banner.style.display = "block";
+    banner.innerText = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+}
+
+
 //Full year
 document.querySelector("#year").textContent = new Date().getFullYear();
 
