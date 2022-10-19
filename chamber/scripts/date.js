44e71bea537f52cad7119112fadb80fe20026135
Marychel <cal20024@byui.edu>
2022-10-19 11:41:56 +0900

    update for week 5

--- chamber/scripts/date.js
+++ chamber/scripts/date.js
@@ -11,15 +11,6 @@ menuBtn.addEventListener('click', () =>
 
 window.onresize = () => {if (window.innerWidth > 760) mnav.classList.remove('open')};
 
-// set banner for monday/tuesday
-let dow = date.getDay();
-if (dow == 1 | dow ==2) {
-    let wednesday = document.getElementById("wednesday");
-    wednesday.style.display = "block";
-    wednesday.innerText = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
-}
-
-
 //Full year
 document.querySelector("#year").textContent = new Date().getFullYear();
 
@@ -54,3 +45,10 @@ let year = d.getFullYear();
 document.querySelector('.currentDate').innerHTML = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
 document.querySelector('#currentDate').innerHTML = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
 
+// set banner for monday/tuesday
+let dow = date.getDay();
+if (dow == 1 | dow ==2) {
+    let banner = document.getElementById("banner");
+    banner.style.display = "block";
+    banner.innerText = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+}
