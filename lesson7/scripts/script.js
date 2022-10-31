ae55bc57d0597d94f0f05e8b8633538ce16775af
Marychel <cal20024@byui.edu>
2022-10-31 23:15:43 +0900

    update week 7

--- lesson7/scripts/script.js
+++ lesson7/scripts/script.js
@@ -31,3 +31,48 @@ const imgObserver = new IntersectionObserver((entries, imgObserver) => {
  img.forEach(img => {
   imgObserver.observe(img);
 })
+
+/*Local storage*/
+//get the current date
+//store in the localeStorage the date user visit the site
+//determine how many days it has been user visited
+//display message to the user or welcome message
+
+const currentDateInMilli =Date.now();
+const lastDate = localStorage.getItem("last-visit");
+
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
+
+
+// increment the number of visits.
+numVisits++;
+// store the new number of visits value
+localStorage.setItem("visits-ls", numVisits);
+
+if (lastDate === undefined){
+    localStorage.setItem("last-visit", currentDateInMilli);
+}else{
+ 
+const ld = parseFloat(lastDate);
+
+const days_ago = (currentDateInMilli - ld) / 86400000;
+const result = days_ago.toFixed(0);
+
+let day;
+  if (result > 1){
+    day = "days"
+  }else{
+    day = "day"
+  }
+let message;
+ if (result >= 1 && numVisits >= 1){
+  message = `You have visited this page for ${numVisits} times. It's been ${result} ${day} since your last visit.`; 
+ }else if(result == 0 && numVisits >= 1){
+  message = `Welcome to Akarui Mirai! You have visited this page for ${numVisits} times. `;}
+ else{
+  message = "Welcome to Akarui Mirai!"
+ }
+localStorage.setItem("last-visit", currentDateInMilli);
+document.querySelector('.localstorage').innerHTML = message;
+
+}
