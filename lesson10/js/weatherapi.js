d494b7a15735ce2efe09f494fb8fbc64dab59952
Marychel <cal20024@byui.edu>
2022-11-16 13:48:12 +0900

    update directory week 9

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,31 +1,10 @@
-const currentTemp = parseInt(document.getElementById('temperature').innerText);
-
-const currentWindSpeed = parseInt(document.querySelector('.wind_speed').innerText);
-
-let currentWindChill = "";
-if (currentTemp <= 50 & currentWindSpeed > 3) {
-    currentWindChill = Math.round(35.74 + (0.6215 * currentTemp ) - 
-                    (35.75 * (currentWindSpeed ** 0.16)) + 
-                    (0.4275 * currentTemp * (currentWindSpeed ** 0.16) )).toString() + "°F";
-
-}
-
-else {
-    currentWindChill = "N/A"
-}
-
-document.querySelector('.wind_chill').innerText = String(currentWindChill)
-
-//current weather for Tsu
 // select HTML elements in the document
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
-//const wind_speed = document.querySelector(`windchillid`);
-//wind_speed.innerHTML=`test`;
 
 const apikey="8dc35cc72784031c27cc5b029ed372e1"
-const url = 'https://api.openweathermap.org/data/2.5/weather?q=Tsu&units=imperial&appid='+apikey;
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid='+apikey;
 
 
 async function apiFetch() {
