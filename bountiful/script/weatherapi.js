e258c62b85d5e82e2388fe8386db804889ee92c3
Marychel <cal20024@byui.edu>
2022-12-03 18:43:20 +0900

    update

---
+++ bountiful/script/weatherapi.js
@@ -0,0 +1,36 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const apikey="8dc35cc72784031c27cc5b029ed372e1"
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid='+apikey;
+
+
+async function apiFetch() {
+     try {
+       const response = await fetch(url);
+       if (response.ok) {
+         const data = await response.json();
+         console.log(data); //this is for testing the call
+          displayResults(data);
+       } else {
+           throw Error(await response.text());
+       }
+     } catch (error) {
+         console.log(error);
+     }
+   }
+   
+   apiFetch();
+
+   function  displayResults(weatherData) {
+     currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+   
+     const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+     const desc = weatherData.weather[0].description;
+   
+     weatherIcon.setAttribute('src', iconsrc);
+     weatherIcon.setAttribute('alt', desc);
+     captionDesc.textContent = desc;
+   }
