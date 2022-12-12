937fac54256ae53a5232c7e2e746aa8807776167
Marychel <cal20024@byui.edu>
2022-12-13 00:56:14 +0900

    update bountiful

--- bountiful/script/index.js
+++ bountiful/script/index.js
@@ -1,3 +1,27 @@
+//current date
+let day = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday'
+];
+let month = [
+  'January',
+  'Febuary',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
 let d = new Date();
 let dayName = day[d.getDay()];
 let monthName = month[d.getMonth()];
