045fff6e0df4e6ae24db102872413e4e80d002bc
Marychel <cal20024@byui.edu>
2022-12-15 02:21:55 +0900

    about.html

---
+++ bountiful/script/about.js
@@ -0,0 +1,48 @@
+//GETS TOGGLE MENU| Header
+const menuBtn = document.querySelector('.hamBtn');
+const mnav = document.querySelector('.navi');
+
+menuBtn.addEventListener('click', () =>
+{mnav.classList.toggle('open')}, false);
+
+menuBtn.addEventListener('click', () =>
+{menuBtn.classList.toggle('open')}, false);
+
+
+window.onresize = () => {if (window.innerWidth > 760) mnav.classList.remove('open')};
+
+//Full year
+document.querySelector("#year").textContent = new Date().getFullYear();
+
+
+//current date
+let day = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday'
+];
+let month = [
+  'January',
+  'Febuary',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
+let d = new Date();
+let dayName = day[d.getDay()];
+let monthName = month[d.getMonth()];
+let year = d.getFullYear();
+// document.querySelector('.currentDate').innerHTML = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
+document.getElementById("lastModif").innerHTML = `Last Updated:  ${document.lastModified}`;
+
--- bountiful/script/index.js
+++ bountiful/script/index.js
@@ -1,3 +1,20 @@
+//GETS TOGGLE MENU| Header
+const menuBtn = document.querySelector('.hamBtn');
+const mnav = document.querySelector('.navi');
+
+menuBtn.addEventListener('click', () =>
+{mnav.classList.toggle('open')}, false);
+
+menuBtn.addEventListener('click', () =>
+{menuBtn.classList.toggle('open')}, false);
+
+
+window.onresize = () => {if (window.innerWidth > 760) mnav.classList.remove('open')};
+
+//Full year
+document.querySelector("#year").textContent = new Date().getFullYear();
+
+
 //current date
 let day = [
   'Sunday',
@@ -26,5 +43,62 @@ let d = new Date();
 let dayName = day[d.getDay()];
 let monthName = month[d.getMonth()];
 let year = d.getFullYear();
-document.querySelector('.currentDate').innerHTML = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
+// document.querySelector('.currentDate').innerHTML = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
 document.getElementById("lastModif").innerHTML = `Last Updated:  ${document.lastModified}`;
+
+//elements
+
+const icon = document.querySelector('.icon');
+const desc = document.querySelector('.desc');
+const currentemp = document.querySelector('.currentemp');
+const hum = document.querySelector('.hum');
+
+
+
+    const weatherApi = 'https://api.openweathermap.org/data/2.5/onecall?lat=34.5&lon=136.5&exclude=hourly,minutely&units=imperial&appid=a44cb97f9caa00ac7b9a9561a8379fe8';
+    fetch(weatherApi)
+        .then(function(response){
+            let data = response.json();
+            
+            return data;
+            
+        })
+        .then(function(data){
+
+            
+            const icon = document.querySelector('.icon');
+            const desc = document.querySelector('.desc');
+            const currentemp = document.querySelector('.currentemp');
+            const hum = document.querySelector('.hum');
+            let tempA = data.current.temp.toFixed(0);
+            let image = `images/weather-icons/${data.current.weather[0].icon}.png`;
+            desc.innerHTML = data.current.weather[0].description;
+            descript = data.current.weather[0].description;
+            currentemp.innerHTML = `${tempA}°F`;
+            hum.innerHTML = `<i class="fas fa-tint"> ${data.current.humidity}%`;
+            icon.setAttribute('src', image);
+            icon.setAttribute('alt', descript);
+            console.log(desc)
+            const three_day = data.daily.slice(0,3);
+           
+            
+
+            let day = 0;
+            three_day.forEach(forecast =>{
+               let imagesrc = `images/weather-icons/${forecast.weather[0].icon}.png`; 
+               let description = forecast.weather[0].description;
+               let time = forecast.dt;
+               const options = {month: 'short', day: 'numeric' };
+               let date = new Date(time * 1000).toLocaleDateString(undefined, options);
+               
+                
+                document.querySelector(`.date${day + 1}`).textContent = date;
+                document.querySelector(`.temp${day + 1}`).textContent = `${forecast.temp.day.toFixed(0)} °F`;
+                document.querySelector(`.icon${day + 1}`).setAttribute('src', imagesrc);
+                document.querySelector(`.icon${day + 1}`).setAttribute('alt', description);
+                day++;
+            
+
+    
+        });
+    })
